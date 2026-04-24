784a0301d96eaf8d8f1168c0a0609ec7ca938583
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2026-04-24 11:43:14 +0100

    Add comments to Figure_S9_Heatmap_GillCiliaryCells.R
    Added comments explaining the purpose and output of the script.

---
+++ scripts/06_figures/Supplementary_figures/Figure_S9_Heatmap_GillCiliaryCells.R
@@ -0,0 +1,6 @@
+# Please see the script scripts/06_figures/Main_figures/Figure_7_DEGs_heatmap.R
+# The script above does batch-plotting for all clusters in one go !
+# Heatmaps are saved in:
+# ├── heatmaps_avgexp_top15
+#     ├── up_in_control
+#     ├── up_in_target
